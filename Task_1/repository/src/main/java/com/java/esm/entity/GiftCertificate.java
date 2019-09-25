9688675eb7b0645a6250d220ca47f90d881afecb
Ali Al-Iusefi <Ali_Al-Iusefi@epam.com>
2019-09-25 11:40:14 +0300

    Added Entities

---
+++ Task_1/repository/src/main/java/com/java/esm/entity/GiftCertificate.java
@@ -0,0 +1,106 @@
+package com.java.esm.entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+
+final public class GiftCertificate extends Entity {
+
+    private String name;
+    private String description;
+    private double price;
+    private LocalDate dateOfCreation;
+    private LocalDate dateOfModification;
+    private int durationTillExpiry;
+
+    public GiftCertificate(long id, String name, String description, double price, LocalDate dateOfCreation,
+                           LocalDate dateOfModification, int durationTillExpiry) {
+        super(id);
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.dateOfCreation = dateOfCreation;
+        this.dateOfModification = dateOfModification;
+        this.durationTillExpiry = durationTillExpiry;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDate getDateOfCreation() {
+        return dateOfCreation;
+    }
+
+    public void setDateOfCreation(LocalDate dateOfCreation) {
+        this.dateOfCreation = dateOfCreation;
+    }
+
+    public LocalDate getDateOfModification() {
+        return dateOfModification;
+    }
+
+    public void setDateOfModification(LocalDate dateOfModification) {
+        this.dateOfModification = dateOfModification;
+    }
+
+    public int getDurationTillExpiry() {
+        return durationTillExpiry;
+    }
+
+    public void setDurationTillExpiry(int durationTillExpiry) {
+        this.durationTillExpiry = durationTillExpiry;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        GiftCertificate that = (GiftCertificate) o;
+        return Double.compare(that.price, price) == 0 &&
+                durationTillExpiry == that.durationTillExpiry &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(dateOfCreation, that.dateOfCreation) &&
+                Objects.equals(dateOfModification, that.dateOfModification);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name, description, price,
+                dateOfCreation, dateOfModification, durationTillExpiry);
+    }
+
+    @Override
+    public String toString() {
+        return "GiftCertificate{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", dateOfCreation=" + dateOfCreation +
+                ", dateOfModification=" + dateOfModification +
+                ", durationTillExpiry=" + durationTillExpiry +
+                '}';
+    }
+}
---
+++ Task_1/repository/src/main/java/com/java/esm/entity/Tag.java
@@ -0,0 +1,42 @@
+package com.java.esm.entity;
+
+import java.util.Objects;
+
+public class Tag extends Entity {
+
+    private String name;
+
+    public Tag(long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(name, tag.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name);
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
