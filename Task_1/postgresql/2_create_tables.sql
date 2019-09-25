8f44b6b46c41d130b020967b520c4a045c013aa5
Ali Al-Iusefi <Ali_Al-Iusefi@epam.com>
2019-09-25 11:39:59 +0300

    Added New Table

--- Task_1/postgresql/2_create_tables.sql
+++ Task_1/postgresql/2_create_tables.sql
@@ -1,15 +1,24 @@
 create table giftcertificates
-(id serial primary key,
- name varchar(16),
- description text,
- price float,
- date_created date ,
- date_modified date ,
- duration_till_expiry integer
- );
-
-create table tag (
-  id serial primary key,
-  tag_name varchar(16),
-  gift_certificate_id integer references giftcertificates(id)
+(
+    id                   serial primary key,
+    name                 varchar(16),
+    description          text,
+    price                float,
+    date_created         date,
+    date_modified        date,
+    duration_till_expiry integer
 );
+
+create table tag
+(
+    id       serial primary key,
+    tag_name varchar(16)
+);
+
+create table tagged_giftcertificates
+(
+    tag_id              integer references tag (id),
+    gift_certificate_id integer references giftcertificates (id)
+);
+
+
