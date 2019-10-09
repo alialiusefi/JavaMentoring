cbed2c8565256eb6025ad0865926f5ec1aa2d320
Ali Al-Iusefi <Ali_Al-Iusefi@epam.com>
2019-10-09 18:47:20 +0300

    Fixed AddReference Bug, update certifcate bug, more table filling

---
+++ Task_1/repository/src/test/java/com/epam/esm/repository/TagRepoTest.java
@@ -0,0 +1,44 @@
+package com.epam.esm.repository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TagRepoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void findByID() {
+    }
+
+    @Test
+    public void query() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void getFieldsArray() {
+    }
+}
---
+++ Task_1/service/src/main/java/com/epam/esm/service/IService.java
@@ -0,0 +1,8 @@
+package com.epam.esm.service;
+
+import com.epam.esm.repository.CRUDRepo;
+
+public interface IService<T extends CRUDRepo> {
+
+
+}
