b9fe684dd29e1083394920fea65de6b256e570ed
Ali Al-Iusefi <Ali_Al-Iusefi@epam.com>
2019-10-16 11:44:11 +0300

    Embedded PostgresSQL, New Tag Tests

--- Task_1/repository/src/test/java/com/epam/esm/repository/TagRepoTest.java
+++ Task_1/repository/src/test/java/com/epam/esm/repository/TagRepoTest.java
@@ -1,41 +1,49 @@
 package com.epam.esm.repository;
 
 import com.epam.esm.entity.Tag;
+import com.epam.esm.repository.specfication.FindTagByName;
+import com.epam.esm.repository.specfication.FindTagsByCertificateID;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@Transactional
-public class TagRepoTest extends AbstractRepoTest<Tag>{
-
-    @Autowired
-    TagRepository tagRepository;
-
-    public TagRepoTest() {
-    }
+public class TagRepoTest extends AbstractRepoTest {
 
     @Test
     public void findByID() {
-        Tag expected = new Tag(1,"Accessories");
-        Tag actual = tagRepository.findByID(1);
-        assertEquals(expected,actual);
+        assertTrue(true);
     }
 
+    @Test
+    public void findAllByName() {
+        List<Tag> expectedTags = Arrays.asList(new Tag(1, "Accesories"));
+        List<Tag> actualTags = tagRepository.query(new FindTagByName("Accesories"));
+        assertEquals(expectedTags, actualTags);
+    }
+
+    @Test
+    public void findAllByCertificateID() {
+        List<Tag> expectedTags = Arrays.asList(new Tag(1, "Accesories"), new Tag(2, "Food"));
+        List<Tag> actualTags = tagRepository.query(new FindTagsByCertificateID(1));
+        assertEquals(expectedTags, actualTags);
+    }
 
     @Test
     public void add() {
+        Tag expectedTag = new Tag();
+        expectedTag.setName("TestTag");
+        Tag actual = tagRepository.add(expectedTag);
+        assertEquals(expectedTag, actual);
     }
 
     @Test
     public void update() {
-    }
 
+    }
 
     @Test
     public void query() {
@@ -49,7 +57,4 @@ public class TagRepoTest extends AbstractRepoTest<Tag>{
     public void testDelete() {
     }
 
-    @Test
-    public void getFieldsArray() {
-    }
 }
---
+++ Task_1/repository/src/test/resources/postgresql/2_create_tables.sql
@@ -0,0 +1,35 @@
+create table giftcertificates
+(
+    id                   serial primary key,
+    name                 varchar(50),
+    description          text,
+    price                float,
+    date_created         date,
+    date_modified        date,
+    duration_till_expiry integer
+);
+
+create table tag
+(
+    id       serial primary key,
+    tag_name varchar(16)
+);
+
+create table tagged_giftcertificates
+(
+    tag_id              integer references tag (id) on delete cascade,
+    gift_certificate_id integer references giftcertificates (id) on delete cascade
+);
+
+CREATE FUNCTION public.consists(IN a text, IN b text)
+    RETURNS boolean
+    LANGUAGE 'plpgsql'
+AS
+$BODY$
+begin
+    return b like '%' || a || '%';
+end;
+$BODY$;
+
+ALTER FUNCTION public.consists(text, text)
+    OWNER TO postgres;
