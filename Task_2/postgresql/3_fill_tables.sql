cdc8aa1f9aa545795af262081f4cf7a4f090e3e2
Ali Al-Iusefi <Ali_Al-Iusefi@epam.com>
2019-10-27 12:50:41 +0300

    Update fill_tables sql file

--- Task_2/postgresql/3_fill_tables.sql
+++ Task_2/postgresql/3_fill_tables.sql
@@ -1,12 +1,12 @@
-insert into tag(tag_name)
-values ('Accessories'),
-       ('Food'),
-       ('Hotel'),
-       ('Travel'),
-       ('Free'),
-       ('Sale'),
-       ('English'),
-       ('Procrastination');
+insert into tag(id, tag_name)
+values (1,'Accessories'),
+       (2,'Food'),
+       (3,'Hotel'),
+       (4,'Travel'),
+       (5,'Free'),
+       (6,'Sale'),
+       (7,'English'),
+       (8,'Procrastination');
 
 insert into giftcertificates(name, description, price, date_created, date_modified, duration_till_expiry)
 VALUES ('ACME Discount Voucher', 'Discount while shopping', 20.00, '2012-09-09', '2014-12-01', 5),
