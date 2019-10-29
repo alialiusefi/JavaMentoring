d1173f0f82c65d431d4a7866f2ea5942afda924c
Ali Al-Iusefi <Ali_Al-Iusefi@epam.com>
2019-10-29 13:42:04 +0300

    Added Users, Authorities, Orders table, username authority index

--- Task_2/postgresql/2_create_tables.sql
+++ Task_2/postgresql/2_create_tables.sql
@@ -21,6 +21,32 @@ create table tagged_giftcertificates
     gift_certificate_id integer references giftcertificates (id) on delete cascade
 );
 
+create table users
+(
+    username varchar(50) not null primary key,
+    password varchar(50) not null,
+    enabled  boolean     not null
+);
+
+create table authorities
+(
+    username  varchar(50) not null,
+    authority varchar(50) not null,
+    constraint fk_authorities_users foreign key (username) references users (username)
+);
+create unique index ix_auth_username on authorities (username, authority);
+
+create table orders
+(
+    id                 serial primary key,
+    username           varchar(50) not null,
+    giftcertificate_id integer     not null,
+    ordercost          float       not null,
+    timestamp          timestamp   not null,
+    constraint fk_orders_users foreign key (username) references users (username),
+    constraint fk_orders_giftcertificates foreign key (giftcertificate_id) references giftcertificates (id)
+);
+
 
 CREATE FUNCTION public.consists(IN a text, IN b text)
     RETURNS boolean
