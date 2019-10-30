f0401a69540371c225d2b7d7f598ac33f4da96a8
Ali Al-Iusefi <Ali_Al-Iusefi@epam.com>
2019-10-30 13:30:38 +0300

    added entities with builders and mapping

--- Task_2/repository/src/main/java/com/epam/esm/entity/Authority.java
+++ Task_2/repository/src/main/java/com/epam/esm/entity/Authority.java
@@ -1,16 +1,80 @@
 package com.epam.esm.entity;
 
-public enum Authority {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
 
-    ADMIN(1), USER(2), GUEST(3);
+@Entity
+@Table
+public class Authority extends AbstractEntity {
 
-    private Integer value;
+    @Id
+    @Column(name = "username", nullable = false)
+    private String username;
 
-    Authority(Integer value) {
-        this.value = value;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "userstatus", nullable = false)
+    private UserStatus userStatus;
+
+    private Authority(AuthorityBuilder builder) {
+        this.username = builder.username;
+        this.userStatus = builder.userStatus;
     }
 
-    public Integer getValue() {
-        return value;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public UserStatus getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(UserStatus userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Authority authority = (Authority) o;
+        return Objects.equals(username, authority.username) &&
+                userStatus == authority.userStatus;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, userStatus);
+    }
+
+    @Override
+    public String toString() {
+        return "Authority{" +
+                "username='" + username + '\'' +
+                ", userStatus=" + userStatus +
+                '}';
+    }
+
+    public static class AuthorityBuilder {
+
+        private final String username;
+        private final UserStatus userStatus;
+
+        public AuthorityBuilder(String username, UserStatus status) {
+            this.username = username;
+            this.userStatus = status;
+        }
+
+        public Authority getResult() {
+            return new Authority(this);
+        }
     }
 }
--- Task_2/repository/src/main/java/com/epam/esm/entity/Order.java
+++ Task_2/repository/src/main/java/com/epam/esm/entity/Order.java
@@ -1,5 +1,137 @@
 package com.epam.esm.entity;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
 public class Order extends AbstractEntity {
+
+    @Id
+    @SequenceGenerator(name = "tagSequence", sequenceName = "tag_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tagSequence")
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "ordercost", nullable = false)
+    private BigDecimal orderCost;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    @ManyToMany
+    @JoinTable(name = "order_giftcertificate", joinColumns =
+            {@JoinColumn(name = "order_id")},
+            inverseJoinColumns = {@JoinColumn(name = "gift_certificate_id")})
+    private List<GiftCertificate> giftCertificateList;
+
+    private Order(OrderBuilder builder) {
+        this.id = builder.id;
+        this.username = builder.username;
+        this.orderCost = builder.orderCost;
+        this.timestamp = builder.timestamp;
+        this.giftCertificateList = builder.giftCertificateList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public BigDecimal getOrderCost() {
+        return orderCost;
+    }
+
+    public void setOrderCost(BigDecimal orderCost) {
+        this.orderCost = orderCost;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public List<GiftCertificate> getGiftCertificateList() {
+        return giftCertificateList;
+    }
+
+    public void setGiftCertificateList(List<GiftCertificate> giftCertificateList) {
+        this.giftCertificateList = giftCertificateList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id) &&
+                Objects.equals(username, order.username) &&
+                Objects.equals(orderCost, order.orderCost) &&
+                Objects.equals(timestamp, order.timestamp) &&
+                Objects.equals(giftCertificateList, order.giftCertificateList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, orderCost, timestamp, giftCertificateList);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", orderCost=" + orderCost +
+                ", timestamp=" + timestamp +
+                ", giftCertificateList=" + giftCertificateList +
+                '}';
+    }
+
+    public static class OrderBuilder {
+
+        private final Long id;
+        private final String username;
+        private final BigDecimal orderCost;
+        private final LocalDateTime timestamp;
+        private final List<GiftCertificate> giftCertificateList;
+
+        public OrderBuilder(Long id, String username, BigDecimal orderCost,
+                            LocalDateTime timestamp, List<GiftCertificate> giftCertificateList) {
+            this.id = id;
+            this.username = username;
+            this.orderCost = orderCost;
+            this.timestamp = timestamp;
+            this.giftCertificateList = giftCertificateList;
+        }
+
+        public Order getResult() {
+            return new Order(this);
+        }
+
+    }
 
 }
