ebce43df0b90d3231d4a450203a6bbff772b145b
Ali Al-Iusefi <Ali_Al-Iusefi@epam.com>
2020-02-14 12:18:13 +0300

    Added tables that should be created and worked on

---
+++ Task_6/1_create_tables.sql
@@ -0,0 +1,44 @@
+create table authors (
+    id bigserial not null,
+    name varchar(48) not null unique
+);
+
+create table news (
+    id bigserial not null,
+    title varchar(100) not null,
+    creation_date date default now() not null,
+    author_id bigint not null,
+    foreign key(author_id) references authors(id) on delete cascade
+);
+
+create table comments (
+    id bigserial not null,
+    content varchar(256) not null,
+    author_id bigint not null,
+    news_id bigint not null,
+    creation_date date default now() not null,
+    foreign key(author_id) references authors(id) on delete cascade,
+    foreign key(news_id) references news(id) on delete cascade
+);
+
+create table tag (
+  id            bigserial primary key,
+  name          varchar(64) unique not null
+);
+
+CREATE TABLE news_to_tag (
+  news_id bigint not null,
+  tag_id  bigint not null,
+  foreign key (tag_id)  references tag (id) on delete cascade,
+  foreign key (news_id) references news (id) on delete cascade,
+  primary key (news_id, tag_id)
+  );
+
+CREATE TABLE logs (
+  id BIGSERIAL primary key,
+  table_name varchar(64) not null,
+  insert_date timestamp not null,
+  description TEXT not null
+);
+
+
