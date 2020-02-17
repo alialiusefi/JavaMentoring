bf881d1d14d58c4e912767622d6cc06a4fcea1e2
Ali Al-Iusefi <Ali_Al-Iusefi@epam.com>
2020-02-17 10:56:23 +0300

    query 8

---
+++ Task_6/QUERY_8.sql
@@ -0,0 +1,4 @@
+
+select segment_name,segment_type,bytes/1024/1024 MB
+from user_segments inner join user_Tables on TABLE_NAME = SEGMENT_NAME
+where segment_type='TABLE' and segment_name = TABLE_NAME /*and owner='C##IDEA'*/;
