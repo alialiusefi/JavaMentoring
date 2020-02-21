64d406e211e7f6a19e9847a1919ace429c0b8135
Ali Al-Iusefi <Ali_Al-Iusefi@epam.com>
2020-02-21 11:13:22 +0300

    used oracle listagg function

--- Task_6/QUERY_4.sql
+++ Task_6/QUERY_4.sql
@@ -4,6 +4,5 @@ select news.id, news.CONTENT,LISTTAGSBYNEWSID(news.id,',') from news;
 
 /*Oracle 10g db feature*/
 select news.id, news.CONTENT,listagg(T.NAME, ',')
-from news inner join NEWS_TO_TAG NTT on NEWS.ID = 1
+from news inner join NEWS_TO_TAG NTT on NEWS.ID = 2
 inner join TAGS T on NTT.TAG_ID = T.ID group by news.id, news.CONTENT;
-
